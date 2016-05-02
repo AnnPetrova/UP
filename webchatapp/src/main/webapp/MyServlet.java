8fbbd12b40d55b5cd87ac1bd5ab90ef22fab9329
AnnPetrova <brigantinas_fan@mail.ru>
2016-05-02 23:46:54 +0300

    add some images

--- webchatapp/src/main/webapp/MyServlet.java
+++ webchatapp/src/main/webapp/MyServlet.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @WebServlet(value = "/vars")
-public class EnvVarsServlet extends HttpServlet {
+public class MyServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
